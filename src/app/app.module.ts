35f22bbad562a875ca6f92071dc3991f22218510
Edson Macamo <edsonmacamo85@gmail.com>
2021-10-24 12:39:58 +0200

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BookComponent } from './book/book.component';
+import { DvdComponent } from './dvd/dvd.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterModule, Routes } from '@angular/router';
 
+
+const appRoutes :Routes = [
+  {path:'dvds', component: DvdComponent},
+  {path:'books', component: BookComponent},
+  {path:'', pathMatch:'full', redirectTo: 'dvds'},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    DvdComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
